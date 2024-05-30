91b90ac2935b5d90cacd1a9dcab85103f71f01a3
Allen Feng <59977850+yfeng445@users.noreply.github.com>
2024-05-31 05:47:26 +0800

    Delete Group Assignment - Final Stage directory

--- Group Assignment - Final Stage/VoiceAPI.cpp
+++
@@ -1,9 +0,0 @@
-// used for sending a curl request to the API
-// here the OpenAI Whisper model is being run at Replicate
-
-#include "VoiceAPI.h"
-
-// get the file from the path and upload it to the API
-void VoiceAPI::upload(std::string &filelPath) {
-    
-}
